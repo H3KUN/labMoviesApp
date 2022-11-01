dbc299c13e0e712d7acdd362b5f258570eda0fbd
Hiroto Iwata <hiroto1610@outlook.com>
2022-11-01 20:11:11 +0000

    Added Upcoming movies page.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import {createRoot} from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
-import {Link} from 'react-router-dom';
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader';
+import UpComingPage from "./pages/upComingPage";
 
 const App = () => {
     return (
@@ -18,6 +18,7 @@ const App = () => {
                 <Route path="/movies/:id" element={<MoviePage />} />
                 <Route path="/" element={<HomePage />} />
                 <Route path="*" element={ <Navigate to="/" /> } />
+                <Route exact path="/movies/upcoming" element={<UpComingPage />} />
             </Routes>
         </BrowserRouter>
     );
